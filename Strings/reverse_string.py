64f221923b6127dd285777592c4f49058224f1f0
Rekhas26 <rekhasampangiramaiah@gmail.com>
2025-04-28 06:43:35 -0700

    Create reverse_string.py

---
+++ Strings/reverse_string.py
@@ -0,0 +1,19 @@
+def reverse_string(s):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        s=list(s)
+        left,right=0,len(s)-1
+        
+        while left<right:
+            s[left],s[right] = s[right],s[left]
+            left+=1
+            right-=1
+        return ''.join(s)
+        
+
+
+# Example usage
+s = 'hello'
+print(reverse_string(s))  #
